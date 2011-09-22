856b77aa41483ac3fcc338128825be23a05c8185
Yeshu Liu <stella@Yeshu-Lius-MacBook-Pro.local>
2011-09-22 23:22:38 +1000

    test

---
+++ project/BookmarkViewController.h
@@ -0,0 +1,16 @@
+//
+//  BookmarkViewController.h
+//  project
+//
+//  Created by Yeshu Liu on 22/09/11.
+//  Copyright 2011 RMIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BookmarkViewController : UIViewController {
+    
+}
+
+@end
